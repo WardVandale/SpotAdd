609dcfac36a1fae4801195893d2e1ece70193853
wardvandale <ward.vandale@ixor.be>
2024-02-14 14:26:16 +0100

    Add transferliked script

--- spotAdd.py
+++ spotAdd.py
@@ -5,13 +5,17 @@ import spotipy
 import time
 from spotipy.oauth2 import SpotifyOAuth
 
+# Initialize isAdded to False and previous track URI to None
+isAdded = False
+previous_track_uri = None
+
 def saveCreds():
-    print("apicreds.json does not exists, creating...")
+    print("apicreds.json does not exist, creating...")
     clientId = input("client id: ")
     clientSecret = input("client secret: ")
     username = input("username: ")
 
-    creds= {
+    creds = {
         "clientId": clientId,
         "clientSecret": clientSecret,
         "username": username
@@ -32,7 +36,6 @@ creds = getCreds()
 client_id = creds["clientId"]
 client_secret = creds["clientSecret"]
 
-# Specify the required scopes
 scopes = [
     'playlist-read-private',
     'playlist-read-collaborative',
@@ -42,25 +45,21 @@ scopes = [
     'playlist-modify-private',
 ]
 
-# Initialize the Spotipy client with the specified scopes
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri='http://localhost:8888/callback', scope=scopes))
 
 user_playlists = sp.current_user_playlists()
 your_playlists = [playlist for playlist in user_playlists['items'] if playlist['owner']['id'] == sp.me()['id']]
 
-# Display your own created playlists and let the user choose one
 print("Your Created Playlists:")
 for i, playlist in enumerate(your_playlists):
     print(f"{i + 1}. {playlist['name']}")
 
 selected_index = int(input("Enter the number of the playlist to add songs to: ")) - 1
 
-# Get the selected playlist ID
 if 0 <= selected_index < len(your_playlists):
     selected_playlist_id = your_playlists[selected_index]['id']
     stopProgram = 0
     while True:
-        # Check the currently playing song
         current_playback = sp.current_playback()
         if current_playback is not None:
             current_track_uri = current_playback['item']['uri']
@@ -69,20 +68,33 @@ if 0 <= selected_index < len(your_playlists):
 
             remaining_playtime_seconds = (current_track_duration_ms - current_track_progress_ms) / 1000
             stopProgram = 0
-            if remaining_playtime_seconds <= 5:
+
+            if previous_track_uri != current_track_uri:
+                # New song started, reset isAdded to False
+                isAdded = False
+                previous_track_uri = current_track_uri
+
+                # Check if the current track URI is already in the playlist
+                playlist_tracks = sp.playlist_tracks(selected_playlist_id)
+                track_uris_in_playlist = [track['track']['uri'] for track in playlist_tracks['items']]
+
+                if current_track_uri in track_uris_in_playlist:
+                    isAdded = True
+
+            if remaining_playtime_seconds <= 5 and not isAdded:
                 playlist_tracks = sp.playlist_tracks(selected_playlist_id)
                 track_uris_in_playlist = [track['track']['uri'] for track in playlist_tracks['items']]
 
                 if current_track_uri not in track_uris_in_playlist:
                     sp.playlist_add_items(selected_playlist_id, [current_track_uri])
                     print(f"Added '{current_playback['item']['name']}' to the playlist.")
+                    isAdded = True  # Mark the song as added
             else:
                 pass
         else:
             stopProgram = stopProgram + 1
             if stopProgram == 600:
                 sys.exit()
-
         time.sleep(1)
 else:
     pass
---
+++ transferLiked.py
@@ -0,0 +1,89 @@
+import os
+import json
+import spotipy
+import sys
+import time
+from spotipy.oauth2 import SpotifyOAuth
+from datetime import datetime
+
+# Initialize isAdded to False and previous track URI to None
+isAdded = False
+previous_track_uri = None
+
+def saveCreds():
+    print("apicreds.json does not exist, creating...")
+    clientId = input("client id: ")
+    clientSecret = input("client secret: ")
+    username = input("username: ")
+
+    creds = {
+        "clientId": clientId,
+        "clientSecret": clientSecret,
+        "username": username
+    }
+    with open('apicreds.json', 'w') as outfile:
+        json.dump(creds, outfile)
+    print("Saved credentials in apicreds.json")
+
+def getCreds():
+    file_exists = os.path.exists("apicreds.json")
+    if not file_exists:
+        saveCreds()
+    with open('apicreds.json') as infile:
+        return json.load(infile)
+
+creds = getCreds()
+
+client_id = creds["clientId"]
+client_secret = creds["clientSecret"]
+
+scopes = [
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'user-read-playback-state',
+    'user-read-currently-playing',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'user-library-read',  # added scope for reading liked songs
+]
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri='http://localhost:8888/callback', scope=scopes))
+
+# Function to create a new playlist with the given name
+def create_playlist(name):
+    user_id = sp.me()['id']
+    playlist = sp.user_playlist_create(user_id, name, public=False)
+    return playlist['id']
+
+# Function to get IDs of all liked songs
+def get_liked_songs():
+    liked_songs = []
+    results = sp.current_user_saved_tracks()
+    while results:
+        for item in results['items']:
+            track = item['track']
+            liked_songs.append(track['id'])
+        if results['next']:
+            results = sp.next(results)
+        else:
+            results = None
+    return liked_songs
+
+# Function to add songs to a playlist given its ID
+# Function to add songs to a playlist given its ID
+def add_songs_to_playlist(playlist_id, song_ids):
+    # Split song_ids into chunks of 100
+    chunks = [song_ids[i:i + 100] for i in range(0, len(song_ids), 100)]
+    
+    # Add songs to the playlist sequentially in chunks
+    for chunk in chunks:
+        sp.playlist_add_items(playlist_id, chunk)
+
+
+if __name__ == "__main__":
+    currentDate = datetime.now().strftime("%Y-%m-%d")
+    liked_songs = get_liked_songs()
+    playlist_name = f"Liked Songs [{currentDate}]"
+    playlist_id = create_playlist(playlist_name)
+    add_songs_to_playlist(playlist_id, liked_songs)
+    print(f"Playlist '{playlist_name}' created with your liked songs.")
